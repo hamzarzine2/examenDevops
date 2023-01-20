0cc29a15ec631b7b06c2b2e6f2c883eb61c55824
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-01-20 10:15:34 +0100

    refactor(validations.js): refractoring: deleting the exports useless

--- src/services/validations.js
+++ src/services/validations.js
@@ -8,4 +8,4 @@ const containsSpecial = (gamerTag)=>new RegExp("[_@./#&+-]+").test(gamerTag);
 const containsNumber = (gamerTag)=> new RegExp("[0-9]+").test(gamerTag);
 const isEmpty = (label) => !label || label.length === 0;
 
-module.exports = {validation,isEmpty,containsNumber};
+module.exports = {validation,isEmpty};
